727100ce2f7311999ef522209f075479a7df1d12
Ralf Vogler <ralf.vogler@gmail.com>
2016-11-15 15:42:32 +0100

    avoid exception accessing .style of null...

--- facebook_hide-topbar-onscroll.js
+++ facebook_hide-topbar-onscroll.js
@@ -4,24 +4,24 @@
 // @version      0.1
 // @author       Ralf Vogler
 // @match        https://*.facebook.com/*
-// @grant        none
+// @grant        GM_addStyle
 // ==/UserScript==
 
 (function() {
     'use strict';
 
+    GM_addStyle('#pagelet_bluebar { position: absolute };');
+    GM_addStyle('#globalContainer { margin-top: 43px };');
     let a = document.body.scrollTop;
-    let s = document.getElementById("pagelet_bluebar").style;
-    s.position = "absolute";
-    document.getElementById("globalContainer").style.marginTop = "43px";
+    let s = document.getElementById("pagelet_bluebar");
     document.addEventListener("scroll", function(){
         let b = document.body.scrollTop;
         if(b<a){
             // console.log("scroll up");
-            s.display = "";
+            s.style.display = "";
         }else{
             // console.log("scroll down");
-            if(b>50) s.display = "none";
+            if(b>50) s.style.display = "none";
         }
         a = document.body.scrollTop;
     }, {passive: true});
